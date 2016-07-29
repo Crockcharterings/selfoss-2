a471bdb1974ae9f2074b6abb55725f738fab0db2
chenbo <1264339504@qq.com>
2016-07-29 19:24:35 +0800

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
-console.log($('title'));
 title = $('title').text();
 desc = "快来看我分享给你的网站:" + title;
 url = window.location.href;
 img = "http://iwebo.portal.net.cn/tongji/icon_small.png";
 guid = $('span').attr('value');
+if(typeof(JsInterface) != "undefined"){
+    JsInterface.setPageUrl(url);        
+    JsInterface.setPageTitle(title);        
+    JsInterface.setPageDesc(desc);        
+    JsInterface.setPageLogo(img);
+}
--- public/js/app2.js
+++ public/js/app2.js
@@ -1,27 +1,4 @@
-public void setGuid(String guid){
-	dp.setGuid(guid);
-}
-public String getGuid(){
-	return dp.getGuid();
-}
-public void setPageUrl(String url){
-	dp.setUrl(url);
-}
-public void setPageTitle(String title){
-	dp.setTitle(title);
-}
-public void setPageDesc(String desc){
-	dp.setDesc(desc);
-}
-public void setPageLogo(String img){
-	Bitmap bmp = getBitmapFromUrl(img);
-	dp.setImg(bmp);
-}
 if(typeof(JsInterface) != "undefined"){
 	void guid = JsInterface.getGuid();
 	JsInterface.setGuid(guid);
-	JsInterface.setGuid(url);
-	JsInterface.setGuid(title);
-	JsInterface.setGuid(desc);
-	JsInterface.setGuid(img);
 }
