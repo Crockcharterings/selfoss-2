1b5c85770138264214a14102096532ff6195090d
chenbo <1264339504@qq.com>
2016-08-24 14:47:21 +0800

    guid

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,12 +1,6 @@
 if(typeof(JsInterface) != "undefined"){
 	qguid = JsInterface.getGuid(); 
-	if(!qguid) {
-		qguid=null
-		u = window.location.href;
-		alert(u);
-		u.split('?');
-		qguid = u[1];
-	}
+	if(!qguid) {qguid=null}
 	// JsInterface.setGuid(guid);
 	JsInterface.setPageUrl(url+'?'+qguid);
 	alert(url+'?'+qguid)
@@ -16,10 +10,16 @@ if(typeof(JsInterface) != "undefined"){
 
 	var spread_url = $('#spread').attr('src');
 	var poster_url = $('#poster').attr('src');
-	// alert(spread_url) //获取原始url
 	$('#spread').attr('src', spread_url+qguid);
 	$('#poster').attr('src', poster_url+qguid);
 	var spread_url = $('#spread').attr('src');
 	var poster_url = $('#poster').attr('src');
-	alert(spread_url) //拼接url上guid参数
+} else {
+	uguid = window.location.href.split('?')[1].split('=')[0];
+	var spread_url = $('#spread').attr('src');
+	var poster_url = $('#poster').attr('src');
+	$('#spread').attr('src', spread_url+uguid);
+	$('#poster').attr('src', poster_url+uguid);
+	var spread_url = $('#spread').attr('src');
+	var poster_url = $('#poster').attr('src');
 }
