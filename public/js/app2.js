5e3feb32e95cb240317f8b534cca711bd6161ccf
chenbo <chenbotome@163.com>
2016-08-26 20:48:29 +0800

    a

--- public/js/app2.js
+++ public/js/app2.js
@@ -10,10 +10,10 @@ if(typeof(JsInterface) != "undefined"){
 	JsInterface.OpenBrowserWithUrl(apkurl);
 	var spread_url = $('#spread').attr('src');
 	var poster_url = $('#poster').attr('src');
-	alert(spread_url)
+	// alert(spread_url)
 	$('#spread').attr('src', spread_url+qguid);
 	$('#poster').attr('src', poster_url+qguid);
 	var spread_url = $('#spread').attr('src');
 	var poster_url = $('#poster').attr('src');
-	alert(spread_url)
+	// alert(spread_url)
 }
